4e0c669416a467286a61c3930d3a34f9675130ee
duon97ran2 <duongtaph13276@fpt.edu.vn>
2021-11-18 10:24:31 +0700

    first commit

---
+++ views/admin/account/index.php
@@ -0,0 +1,29 @@
+<h2>Danh sách tài khoản</h2>
+<a href="<?= ADMIN_URL ?>tai-khoan/tao-moi">Thêm tài khoản</a>
+<table class="table table-stripped">
+    <thead>
+        <th>ID</th>
+        <th>Họ và tên</th>
+        <th>Email</th>
+        <th>Ảnh đại diện</th>
+        <th>
+
+        </th>
+    </thead>
+    <tbody>
+        <?php foreach ($dsTaiKhoan as $u) : ?>
+            <tr>
+                <td><?= $u['id'] ?></td>
+                <td><?= $u['name'] ?></td>
+                <td><?= $u['email'] ?></td>
+                <td>
+                    <img src="<?= PUBLIC_ASSETS . $u['avatar'] ?>" width="100">
+                </td>
+                <td>
+                    <a href="<?= ADMIN_URL . 'tai-khoan/sua?id=' . $u['id'] ?>" class="btn btn-sm btn-info">Sửa</a>
+                    <a href="<?= ADMIN_URL . 'tai-khoan/xoa?id=' . $u['id'] ?>" class="btn btn-sm btn-danger">Xóa</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
