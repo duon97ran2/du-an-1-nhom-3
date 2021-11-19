a3d6d2c6d4aa07f121670edf3e79dd69912f7d75
duon97ran2 <duongtaph13276@fpt.edu.vn>
2021-11-19 23:14:55 +0700

    Update edit-form.php

--- views/admin/account/edit-form.php
+++ views/admin/account/edit-form.php
@@ -15,6 +15,41 @@
             <label for="">Ảnh đại diện</label>
             <input type="file" name="avatar" id="" class="form-control" placeholder="">
 
+            <div class="form-group">
+                <label for="">Ảnh đại diện</label>
+                <input type="file" name="avatar" id="" class="form-control" placeholder="">
+            </div>
+            <div class="form-group">
+                <label for="status">Trạng thái</label>
+                <select name="status" id="">
+                    <option value="0" <?php echo ($user['status']==0)?'selected':''?>>Kích hoạt</option>
+                    <option value="1" <?php echo ($user['status']==1)?'selected':''?>>Tạm khóa</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="role_id">Vai trò</label>
+                <select name="role_id" id="">
+                    <option value="0" <?php echo ($user['role_id']==0)?'selected':''?>>Quản trị viên</option>
+                    <option value="1" <?php echo ($user['role_id']==1)?'selected':''?>>Nhân viên</option>
+                    <option value="2" <?php echo ($user['role_id']==2)?'selected':''?>>Khách hàng</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="gender">Giới tính</label>
+                <select name="gender" id="">
+                    <option value="0" <?php echo ($user['gender']==0)?'selected':''?>>Nam</option>
+                    <option value="1" <?php echo ($user['gender']==1)?'selected':''?>>Nữ</option>
+                    <option <?php echo ($user['gender']==2)?'selected':''?>value="2">Khác</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="phone">Số điện thoại</label>
+                <input type="text" name="phone" id="" class="form-control" placeholder="" value="<?=$user['phone']?>">
+            </div>
+            <div class="form-group">
+                <label for="address">Địa chỉ</label>
+                <input type="text" name="address" id="" class="form-control" placeholder="" value="<?=$user['address']?>">
+            </div>
         </div>
     </div>
     <br>
