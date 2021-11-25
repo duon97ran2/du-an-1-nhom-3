a79706b7f828d9cfc2cce7968c43d34db20164c2
duon97ran2 <duongtaph13276@fpt.edu.vn>
2021-11-25 14:21:14 +0700

    update

--- commons/app_config.php
+++ commons/app_config.php
@@ -14,8 +14,8 @@ const MAIL_HOST = 'smtp.gmail.com';
 const MAIL_SMTP_AUTH = true;
 const MAIL_SMTP_SECURE = 'tls';
 const MAIL_PORT = '587';
-const MAIL_USERNAME = '';
-const MAIL_PASSWORD = '';
+const MAIL_USERNAME = 'manutd0097@gmail.com';
+const MAIL_PASSWORD = 'Vodoi001';
 
 
 ?>
